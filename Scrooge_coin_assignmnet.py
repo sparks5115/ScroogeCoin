7b99d4625eee129b5b163a4539e34970ffab25b1
Sam <sparks@wpi.edu>
2023-02-14 11:08:55 -0500

    some work

--- Scrooge_coin_assignmnet.py
+++ Scrooge_coin_assignmnet.py
@@ -1,11 +1,16 @@
+import binascii
+import base58
 import hashlib
 import json
 from fastecdsa import ecdsa, keys, curve, point
 
 class ScroogeCoin(object):
     def __init__(self):
-        self.private_key, self.public_key = # MUST USE secp256k1 curve from fastecdsa
-        self.address = # create the address using public key, and bitwise operation, may need hex(value).hexdigest()
+        self.private_key, self.public_key = createKeyPair()
+        print("Private Key: ", self.private_key)
+        print("Public Key: ", self.public_key)
+        
+        self.address = createAddress(self.public_key)
         self.chain = [] # list of all the blocks
         self.current_transactions = [] # list of all the current transactions
 
@@ -16,13 +21,13 @@ class ScroogeCoin(object):
         """
 
         tx = {
-            "sender" : # address,
+            "sender" : -1, # TODO is this correct?
             # coins that are created do not come from anywhere
             "location": {"block": -1, "tx": -1},
             "receivers" : receivers,
         }
-        tx["hash"] = # hash of tx
-        tx["signature"] = # signed hash of tx
+        tx["hash"] = self.hash(tx)
+        tx["signature"] = self.sign(tx["hash"])
 
         self.current_transactions.append(tx)
 
@@ -34,10 +39,11 @@ class ScroogeCoin(object):
         # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
         # use json.dumps().encode() and specify the corrent parameters
         # use hashlib to hash the output of json.dumps()
-        return # hash_of_blob
+        dump = json.dumps(blob, sort_keys=True, skipkeys=True)
+        return hashlib.sha256(dump.encode('utf-8')).hexdigest()
 
     def sign(self, hash_):
-        return # use fastecdsa library
+        return ecdsa.sign(hash_, self.private_key, curve.secp256k1)
 
     def get_user_tx_positions(self, address):
         """
@@ -74,14 +80,22 @@ class ScroogeCoin(object):
 
         :return: if tx is valid return tx
         """
-        is_correct_hash = False
-        is_signed = False
-        is_funded = False
-        is_all_spent = False
-        consumed_previous = False
+        is_correct_hash = tx["hash"] == self.hash(tx) #TODO hash of just some parts?
+        is_signed = ecdsa.verify(tx["signature"], tx["hash"], public_key, curve.secp256k1)
+        is_funded = True # TODO
+        is_all_spent = True #TODO
+        consumed_previous = True #TODO
 
         if (is_correct_hash and is_signed and is_funded and is_all_spent and not consumed_previous):
-            return tx
+            return True
+        else:
+            print("Transaction is invalid")
+            print("is_correct_hash: ", is_correct_hash)
+            print("is_signed: ", is_signed)
+            print("is_funded: ", is_funded)
+            print("is_all_spent: ", is_all_spent)
+            print("consumed_previous: ", consumed_previous)
+            return False
 
 
     def mine(self):
@@ -90,18 +104,18 @@ class ScroogeCoin(object):
         """
 
         block = {
-            'previous_hash': # previous_hash,
-            'index': # index,
-            'transactions': # transactions,
+            'previous_hash': hash(self.chain[-1] if len(self.chain)>0 else 0),
+            'index': len(self.chain),
+            'transactions': self.current_transactions,
         }
         # hash and sign the block
-        tx["hash"] = # hash of block
-        tx["signature"] = # signed hash of block
+        block["hash"] = self.hash(block)
+        block["signature"] = self.sign(block["hash"])
 
         return block
 
     def add_tx(self, tx, public_key):
-         """
+        """
         checks that tx is valid
         adds tx to current_transactions
 
@@ -117,6 +131,11 @@ class ScroogeCoin(object):
 
         :return: True if the tx is added to current_transactions
         """
+        if(self.validate_tx(tx, public_key)):
+            self.current_transactions.append(tx)
+            return True
+        else:
+            return False
 
     def show_user_balance(self, address):
         """
@@ -132,8 +151,8 @@ class ScroogeCoin(object):
 
 class User(object):
     def __init__(self, Scrooge):
-        self.private_key, self.public_key = # MUST USE secp256k1 curve from fastecdsa
-        self.address = # create the address using public key, and bitwise operation, may need hex(value).hexdigest()
+        self.private_key, self.public_key = createKeyPair()
+        self.address = createAddress(self.public_key)
 
     def hash(self, blob):
         """
@@ -144,11 +163,11 @@ class User(object):
         # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
         # use json.dumps().encode() and specify the corrent parameters
         # use hashlib to hash the output of json.dumps()
-        return # hash_of_blob
+        return hashlib.sha256(json.dumps(blob, sort_keys=True, skipkeys=True).encode()).hexdigest()
 
     def sign(self, hash_):
-        return # use fastecdsa library
-
+        return ecdsa.sign(hash_, self.private_key, curve.secp256k1)
+    
     def send_tx(self, receivers, previous_tx_locations):
         """
         creates a TX to be sent
@@ -157,15 +176,33 @@ class User(object):
         """
 
         tx = {
-                "sender" : # address,
+                "sender" : self.address,
                 "locations" : previous_tx_locations,
                 "receivers" : receivers
             }
 
-        tx["hash"] = # hash of TX
-        tx["signature"] = # signed hash of TX
+        tx["hash"] = self.hash(tx)
+        tx["signature"] = self.sign(tx["hash"])
 
         return tx
+    
+def createKeyPair():
+    ecdsaPrivateKey = keys.gen_private_key(curve.P256)
+    ecdsaPublicKey = keys.get_public_key(ecdsaPrivateKey, curve.P256)
+    return ecdsaPrivateKey, ecdsaPublicKey
+
+    
+def createAddress(ecdsaPublicKey):
+    hash256FromECDSAPublicKey = hashlib.sha256((str(ecdsaPublicKey.x) + str(ecdsaPublicKey.y)).encode('utf-8')).hexdigest()
+    # ridemp160FromHash256 = hashlib.new('ripemd160', binascii.unhexlify(hash256FromECDSAPublicKey))
+    prependNetworkByte = '00' + hashlib.sha256(binascii.unhexlify(hash256FromECDSAPublicKey)).hexdigest() # this should have been ripemd first
+    hash = prependNetworkByte
+    for x in range(1,3):
+        hash = hashlib.sha256(binascii.unhexlify(hash)).hexdigest()
+    cheksum = hash[:8]
+    appendChecksum = prependNetworkByte + cheksum
+    address = base58.b58encode(binascii.unhexlify(appendChecksum))
+    return str(address)
 
 def main():
 
